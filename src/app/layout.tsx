f273f9147b422c5b5040efef9cba2a6b5e8b5bcb
Redbiidd.sun <38393603+redbiiddsun@users.noreply.github.com>
2023-12-12 17:51:44 +0700

    ✨implement navbar & add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,25 @@
-// app/layout.tsx
-import {Providers} from "./providers";
-import './styles/globals.css'
 
-export default function RootLayout({children}: { children: React.ReactNode }) {
+import Navbar from "@/components/Navbar";
+import { Providers } from "./providers";
+import './styles/globals.css'
+import { Metadata } from "next/types";
+import Favicon from "@/app/favicon.ico"
+
+
+export const metadata: Metadata = {
+
+  title: 'redbiiddsun portfolio',
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className='dark'>
+    <html className='white'>
+      <head>
+        <link rel="icon" href="/favicon.png" sizes="" />
+      </head>
       <body>
         <Providers>
+          <Navbar />
           {children}
         </Providers>
       </body>
