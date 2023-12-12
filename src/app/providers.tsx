29619ad5a6040897804b7d62cc308b221569dab4
Redbiidd.sun <38393603+redbiiddsun@users.noreply.github.com>
2023-12-12 17:51:07 +0700

    ✨add ThemeProvider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,17 @@
 'use client'
-
 import { NextUIProvider } from '@nextui-org/react'
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ThemeProviderProps } from "next-themes/dist/types";
 
-export function Providers({children}: { children: React.ReactNode }) {
+export interface ProvidersProps {
+	children: React.ReactNode;
+	themeProps?: ThemeProviderProps;
+}
+
+export function Providers({children, themeProps}: ProvidersProps) {
   return (
     <NextUIProvider>
-      {children}
+      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
     </NextUIProvider>
   )
 }
