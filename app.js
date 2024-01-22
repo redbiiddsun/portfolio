ad1d8218fbb79eb65b02d172fe1d6ae706a2ecc7
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-01-22 23:51:18 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require("express");
+require('dotenv').config()
+const { createServer } = require("http");
+const { parse } = require("url");
 const next = require("next");
 
-const port = parseInt(process.env.PORT, 10) || 3000;
+const port = parseInt("3006", 10);
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = express();
+	createServer((req, res) => {
+		const parsedUrl = parse(req.url, true);
+		handle(req, res, parsedUrl);
+	}).listen(port);
 
-  server.all("*", (req, res) => {
-    return handle(req, res);
-  });
-
-  server.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
-  });
+	console.log(
+		`> Server listening at http://localhost:${port}`
+	);
 });
