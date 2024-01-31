cade3d4b0c270183da409766a482b86fb2f774aa
Redbiidd.sun <38393603+redbiiddsun@users.noreply.github.com>
2024-01-31 15:47:12 +0700

    feat: add a navbar product menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,23 @@
 'use client'
 import React, { useState } from "react";
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, NavbarMenuToggle, NavbarMenu, NavbarMenuItem } from "@nextui-org/react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Dropdown, DropdownTrigger, Button, DropdownMenu, DropdownItem } from "@nextui-org/react";
 import Image from "next/image";
 import { ThemeSwitch } from "./theme-switch";
 import { Icon } from "@iconify/react/dist/iconify.js";
+import { ChevronDown, ExternalLink } from "./icons";
+import { useRouter } from "next/navigation";
 
 
 export default function App() {
+
+    const router = useRouter()
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const menuItems = [
-        {title: "Home", link: "/"},
-        {title: "Works", link: "/works"},
-        {title: "Contact", link: "/"},
-        {title: "Github", link: "https://github.com/redbiiddsun"},
+        { title: "Home", link: "/" },
+        { title: "Works", link: "/works" },
+        { title: "Contact", link: "/" },
+        { title: "Github", link: "https://github.com/redbiiddsun" },
     ];
 
     return (
@@ -35,21 +39,52 @@ export default function App() {
                         {menuItems[0].title}
                     </Link>
                 </NavbarItem>
-                
-                <NavbarItem isActive>
-                    <Link href="/works" aria-current="page">
+
+                <Dropdown>
+                    <NavbarItem>
+                        <DropdownTrigger>
+                            <Button
+                                disableRipple
+                                className="p-0 bg-transparent data-[hover=true]:bg-transparent text-base"
+                                endContent={<ChevronDown fill="currentColor" size={14} />}
+                                radius="sm"
+                                variant="light"
+                            >
+                                Products
+                            </Button>
+                        </DropdownTrigger>
+                    </NavbarItem>
+                    <DropdownMenu
+                        className="w-[340px]"
+                        itemClasses={{
+                            base: "gap-4",
+                        }}
+                    >
+                        <DropdownItem
+                            key="Shortener Link"
+                            description=" Redirects to the original URL, making it easier to share and remember."
+                            startContent={<ExternalLink/>}
+                            onClick={()=>{router.push('https://sl2.redbiiddsun.com/')}}
+                        >
+                            Shortener Link
+                        </DropdownItem>
+                    </DropdownMenu>
+                </Dropdown>
+
+                <NavbarItem >
+                    <Link color="foreground" href="/works">
                         {menuItems[1].title}
                     </Link>
                 </NavbarItem>
                 <NavbarItem>
-                   <a href="https://github.com/redbiiddsun" target="_blank">
+                    <a href="https://github.com/redbiiddsun" target="_blank">
                         {menuItems[3].title}
                     </a>
                 </NavbarItem>
             </NavbarContent>
             <NavbarContent justify="end">
                 <NavbarItem className="lg:flex">
-                    <Icon icon="fa-brands:linkedin" color="#8d8d95" width="22" height="22" onClick={()=>{window.location.replace("https://www.linkedin.com/in/phanasorn-srisayam/")}} />
+                    <Icon icon="fa-brands:linkedin" color="#8d8d95" width="22" height="22" onClick={() => { window.location.replace("https://www.linkedin.com/in/phanasorn-srisayam/") }} />
                 </NavbarItem>
                 <ThemeSwitch />
             </NavbarContent>
